d252a745e78700ce582bd2f790a57b3a78c11ee6
Hafez <a.hafez852@gmail.com>
2018-06-05 18:57:08 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sanitizing object example
+
+Example to a simple sanitizing user input utility for the node best practices repository. Check index.js.
---
+++ index.js
@@ -0,0 +1,37 @@
+const sanitizeObject = ({ object, properties, whitelist, blacklist }) => {
+  if ((!whitelist && !blacklist) || (whitelist && blacklist)) {
+    throw new Error('You have to pick either blacklist or whitelist.');
+  }
+
+  for (let key in object) {
+    if (object.hasOwnProperty(key)) {
+      let removalCondition = whitelist ? !properties.includes(key) : properties.includes(key);
+      if (removalCondition) delete object[key];
+    }
+  }
+};
+
+let objectSentByUser;
+
+
+// whitelist
+objectSentByUser = {
+  a: 'Hello',
+  b: 'Hi there',
+  c: 'Node is fun'
+};
+
+let allowedProperties = ['a', 'b'];
+sanitizeObject({ object: objectSentByUser, properties: allowedProperties, whitelist: true });
+console.log(objectSentByUser); // { a: 'Hello', b: 'Hi there' }
+
+
+// blacklist
+objectSentByUser = {
+  a: 'Hello',
+  b: 'Hi there',
+  c: 'Node is fun'
+};
+let propertiesToRemove = ['a'];
+sanitizeObject({ object: objectSentByUser, properties: propertiesToRemove, blacklist: true });
+console.log(objectSentByUser); // { b: 'Hi there', c: 'Node is fun' }
